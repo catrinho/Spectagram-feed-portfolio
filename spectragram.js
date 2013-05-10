02b5795feddbd3be059fbb3f9e7c9c2ede0e7b82
Matheus <hello@mamomu.com>
2013-05-09 22:28:42 -0300

    Stylizing...

--- spectragram.js
+++ spectragram.js
@@ -42,7 +42,7 @@ if (typeof Object.create !== 'function') {
 		//Get the most recent media published by a user.
         getRecentMedia: function ( userID ) {
 			var self = this,
-				getData = '/users/' + userID + '/media/recent/?' + self.accessData.clientID + '&access_token='+ self.accessData.accessToken +'';
+				getData = '/users/' + userID + '/media/recent/?' + self.accessData.clientID + '&access_token='+ self.accessData.accessToken + '';
 				
                 self.fetch(getData).done(function ( results ) {
                     self.display(results);
@@ -52,7 +52,7 @@ if (typeof Object.create !== 'function') {
 		//Search for a user by name.
         getUserFeed: function () {
 			var self = this,
-				getData = '/users/search?q=' + self.options.query + '&count=' + self.options.max + '&client_id=' + self.accessData.clientID + '';
+				getData = '/users/search?q=' + self.options.query + '&count=' + self.options.max + '&client_id=' + self.accessData.clientID + '' ;
 
 				self.fetch(getData).done(function ( results ) {
 					if(results.data.length){
